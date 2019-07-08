00e33ce25bc3d296ed83aeab72cfd3c5b554c368
Nina Miolane <ninamio78@gmail.com>
2019-07-08 14:12:26 -0700

    Nonetype

--- geomstats/backend/pytorch.py
+++ geomstats/backend/pytorch.py
@@ -21,9 +21,9 @@ def arange(*args, **kwargs):
 
 
 def tril_indices(m, n=None, offset=0):
+    if n is None:
+        n = m
     return torch.ones(m, n, dtype=torch.uint8).tril(offset).nonzero()
-    #if n is None:
-    #    n = m
     #return torch.tril_indices(m, n)
 
 
